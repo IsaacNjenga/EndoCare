d8ac0e5a41229eedf40ab334535ebed83f91a5a9
Isaac Njenga <ayzzoh20@gmail.com>
2024-07-12 06:09:16 +0300

    sf

--- front/src/pages/dashboard.js
+++ front/src/pages/dashboard.js
@@ -9,8 +9,14 @@ function Dashboard() {
       <div className="dashboard">
         <div className="contact-container">
           <h2>Welcome to Your Health Dashboard</h2>
-          <p>Empowering you to take charge of your health journey with personalized resources and support.</p>
-          {/* Add more content or components as needed */}
+          <h3>
+            Empowering you to take charge of your health journey with
+            personalized resources and support.
+          </h3>
+          <br />
+          <p>
+            Your health is in your hands. Let's make positive choices, together!
+          </p>
         </div>
       </div>
     </>
