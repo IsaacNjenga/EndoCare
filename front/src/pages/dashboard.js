61952a2b68d3b67eac4a14b14647f0554434cfb7
Isaac Njenga <ayzzoh20@gmail.com>
2024-07-24 15:20:51 +0300

    landing page

--- front/src/pages/dashboard.js
+++ front/src/pages/dashboard.js
@@ -1,23 +1,41 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import "../assets/css/dashboard.css";
+import doc from "../assets/images/doc.jpg";
 
 function Dashboard() {
-
   return (
     <>
       <Navbar />
       <div className="dashboard">
+        <h1 className="dashboard-title">Welcome to EndoCare</h1>
         <div className="contact-container">
-          <h2>Welcome to Your Health Dashboard</h2>
-          <h3>
-            Empowering you to take charge of your health journey with
-            personalized resources and support.
-          </h3>
-          <br />
-          <p>
-            Your health is in your hands. Let's make positive choices, together!
-          </p>          
+          <div className="welcome-container">
+            <h3>
+              Empowering you to take charge of your health journey with
+              personalized resources and support.
+            </h3>
+            <p>
+              Your health is in your hands. Let's make positive choices,
+              together!
+            </p>
+            <div className="content">
+              <p>
+                EndoCare will provide you with all the tools you need to monitor
+                and manage your health. Explore features like:
+              </p>
+              <ul>
+                <li>Tracking your health metrics</li>
+                <li>Scheduling appointments</li>
+                <li>Accessing personalized health tips</li>
+                <li>Communicating with your healthcare providers</li>
+              </ul>
+              <p>Stay healthy and stay informed!</p>
+            </div>
+          </div>
+          <div className="image-container">
+            <img src={doc} alt="doc" />
+          </div>
         </div>
       </div>
     </>
